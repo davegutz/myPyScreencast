7e73a2535fb66acc0d05dbe4554cf314f8a26840
Dave G <davegutz@users.noreply.github.com>
2024-06-20 11:54:44 -0400

    Update install.py

--- install.py
+++ install.py
@@ -106,12 +106,10 @@ Categories=Utility
         print(Colors.fg.green, "you shouldn't have to remake shortcuts", Colors.reset)
 elif sys.platform == 'darwin':
     print(Colors.fg.green,
-          f"Make sure 'Python Launcher' (Python Script Preferences) option for 'Allow override with #!"
-          f" in script' is checked.\n"
+          f"Make sure 'Python Launcher' (Python Script Preferences) option for 'Allow override with #! in script' is checked.\n"
+          f"in Finder double-click on 'screencast.png'.  Edit-copy the image"
           f"in Finder ctrl-click on 'GUI_screencast.py'\n"
-          f"   - Open and copy icon into paste buffer.\n"
-          f"   - Then 'Get Info' on the duplicate, click on 2nd icon, paste.   Drag item to taskbar.",
-          Colors.reset)
+          f"   - 'Get Info', click on 2nd icon, paste.   Drag item to taskbar.", Colors.reset)
 else:
     print(Colors.fg.green, f"Browse to dist/GUI_screencast.  Make shortcut of .exe and move to Desktop.\n"
                            f"double-click on  'GUI_screencast.exe - Shortcut', set paths on buttons, pin to taskbar",
